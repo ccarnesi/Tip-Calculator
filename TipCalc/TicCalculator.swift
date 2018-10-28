37e68b69801cbc5c664487a5590c2d5fe2c319bc
Chris Carnesi <chriscarnesi@Chriss-MacBook-Pro.local>
2018-10-28 00:26:30 -0400

    First Commit no autolayout

---
+++ TipCalc/TicCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  TicCalculator.swift
+//  TipCalc
+//
+//  Created by Chris Carnesi on 10/27/18.
+//  Copyright © 2018 Carnesi. All rights reserved.
+//
+
+import Foundation
---
+++ TipCalc/TipCalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipCalculatorViewController.swift
+//  TipCalc
+//
+//  Created by Chris Carnesi on 10/27/18.
+//  Copyright © 2018 Carnesi. All rights reserved.
+//
+
+import UIKit
+
+class TipCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
